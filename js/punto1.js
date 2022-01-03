fc9148265fb3d5aee74996f07b0e26a89b4dd694
cristian chaime <ectam484@gmail.com>
2022-01-03 09:08:29 -0300

    terminado

---
+++ js/punto1.js
@@ -0,0 +1,25 @@
+const listadoNotas = [];
+
+let i = 0;
+
+while (i <= 10) {
+  const nota = parseInt(prompt("ingrese una nota"));
+  listadoNotas.push(nota);
+  i++;
+}
+
+let j = 0;
+let cantMayor = 0;
+let cantMenores = 0;
+while (j < listadoNotas.length) {
+  if (listadoNotas[j] >= 7) {
+    cantMayor++;
+  } else {
+    cantMenores++;
+  }
+  j++;
+}
+
+alert(
+  `la cantidad de mayor a 7 es: ${cantMayor} y la cantidad de menores es: ${cantMenores}`
+);
